926a776a11dd783757d4e9ebaf9488f8f166bb6a
Franz Becker <franz.becker@itemis.de>
2018-05-15 20:59:19 +0200

    step 1 - dietfacts example minimal app

---
+++ addons/dietfacts/__openerp__.py
@@ -0,0 +1,8 @@
+{
+    'name' : 'Dietfacts',
+    'version' : '1.0',
+    'description' : 'Adds nutrition information to products',
+    'author' : 'Franz Becker',
+    'depends' : ['sale'],
+    'installable' : True,
+}
---
+++ addons/dietfacts/dietfacts.py
@@ -0,0 +1,1 @@
+# Dietfacts application
