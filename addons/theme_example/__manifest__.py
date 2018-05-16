d86682a053041a1addab16c3cc3d41d971128843
Franz Becker <franz.becker@itemis.de>
2018-05-16 21:30:31 +0200

    create example theme

---
+++ addons/theme_example/__manifest__.py
@@ -0,0 +1,10 @@
+{
+  'name':'Tutorial theme',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Your name',
+
+  'data': [ 'views/assets.xml' ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
